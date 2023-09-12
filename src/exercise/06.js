91bd8fff91e3c41e4f30b54e867f67d067c84dfd
pr-contra <pedro.andrade@gympass.com>
2023-09-12 20:49:39 +0100

    exercise06

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,7 +1,7 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
@@ -19,20 +19,49 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  const inputRef = React.useRef();
+  // const [error, setError] = React.useState(null);
+  const [username, setUsername] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // const username = event.target.username.value
+    // const username = inputRef.current.value;
+
+    onSubmitUsername(username);
+  }
+
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    // const isValidInput = inputValue === inputValue.toLowerCase();
+    // setError(isValidInput ? null : 'Username must be lower case');
+    setUsername(inputValue.toLowerCase());
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username">Username: </label>
+        <input
+          id="username"
+          type="text"
+          ref={inputRef}
+          onChange={handleInputChange}
+          value={username}
+        />
       </div>
+      {/* <button type="submit" disabled={!!error}> */}
       <button type="submit">Submit</button>
+      {/* {error && <p style={{color: 'red', fontWeight: 'bold'}}>{error}</p>} */}
     </form>
-  )
+  );
 }
 
 function App() {
-  const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  const onSubmitUsername = username => alert(`You entered: ${username}`);
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />;
 }
 
-export default App
+export default App;
